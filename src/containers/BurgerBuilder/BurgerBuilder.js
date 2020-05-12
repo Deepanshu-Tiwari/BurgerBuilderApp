ba3f734ecd8824a34a2bc88f680f10889a9483be
Deepanshu Tiwari <deepanshutiwari00@gmail.com>
2020-05-13 03:30:55 +0530

    minor changes

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+// import Aux from "../../hoc/Aux"
+
+
+class BurgerBuilder extends Component{
+    render(){
+        return(
+            <div>
+                <div>Burger</div>
+                <div>BurgerControl</div>
+            </div>
+
+        )
+    }
+}
+
+
+export default BurgerBuilder;
