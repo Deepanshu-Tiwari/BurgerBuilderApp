1bdf54cac8f52a9ec38090b43f91a77938196e78
Deepanshu Tiwari <deepanshutiwari00@gmail.com>
2020-05-13 03:34:42 +0530

    minor changes

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,3 +1,13 @@
-const layout = ()=> {
-    <div>Toolbar, Sidebar, Backdrop</div>
-}
+import React from "react";
+import "../Layout/Layout.css";
+
+const layout = (props)=> (
+    <div>
+    <div >Toolbar, Sidebar, Backdrop</div>
+    <main className="Content">
+        {props.children}
+    </main>
+    </div>
+);
+
+export default layout;
