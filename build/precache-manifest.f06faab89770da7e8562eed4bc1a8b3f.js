cf50cfc261d2fc7de2f334ca35f76dfb86d8c850
Deepanshu Tiwari <deepanshutiwari00@gmail.com>
2020-05-20 07:22:34 +0530

    update changes

---
+++ build/precache-manifest.f06faab89770da7e8562eed4bc1a8b3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86f7b0e7d25aed51e04474908fc29222",
+    "url": "/BurgerBuilderApp/index.html"
+  },
+  {
+    "revision": "1f4a78634a2820432ecc",
+    "url": "/BurgerBuilderApp/static/css/main.eeb4f4c8.chunk.css"
+  },
+  {
+    "revision": "54bc830de1b8ad8d24c5",
+    "url": "/BurgerBuilderApp/static/js/2.a6eac257.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BurgerBuilderApp/static/js/2.a6eac257.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f4a78634a2820432ecc",
+    "url": "/BurgerBuilderApp/static/js/main.ef49a410.chunk.js"
+  },
+  {
+    "revision": "fe40ffb944d857dfbd5d",
+    "url": "/BurgerBuilderApp/static/js/runtime-main.6d52c3c4.js"
+  }
+]);
